311b3c5c88c0083717eb1993bb924ee7630e73a7
WMAlves <williamma03@gmail.com>
2022-10-16 23:32:10 -0300

    Alterado a lógica do arquivo do JS, para que ao clicar no botão mais ou menos de qualquer uma da opções do robo, sejam funcionais

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
-
-
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
-
-const controle=document.querySelectorAll(".controle-ajuste")  //seleciona todos os elementos que possuem a classe controle ajuste 
+const controle = document.querySelectorAll(".controle-ajuste")  //seleciona todos os elementos que possuem a classe controle ajuste 
 
 controle.forEach( (elemento) => { 
     elemento.addEventListener("click",(evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+     
     })
 })
  
-function manipulaDados(operacao){ 
-    if (operacao=="-"){
-        braco.value=parseInt(braco.value)-1
+function manipulaDados(operacao, controle){ 
+
+    const peca = controle.querySelector(".controle-contador")
+
+    if (operacao === "-"){
+        peca.value = parseInt(peca.value)-1
     }else{
-        braco.value=parseInt(braco.value)+1
+        peca.value = parseInt(peca.value)+1
     }
 
 }
 
-// é necessário melhorar o código pois ele está somando apenas para o braço, as outra partes do robo não estão sendo somadas as peças. 
-
-// proxima aula https://cursos.alura.com.br/course/javascript-manipulando-dom/task/106402
-// 3/5 para saber mais: para que server um array.
