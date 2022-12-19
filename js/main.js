558345c094e74d6af23c126cfb3794a80ac32430
WMAlves <williamma03@gmail.com>
2022-12-19 14:19:25 -0300

    atualizado arquivos index.html e main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,48 @@
 const controle = document.querySelectorAll("[data-controle]")  //seleciona todos os elementos que possuem a classe controle ajuste 
+const estatisticas= document.querySelectorAll("[data-estatistica]")
+//console.log(estatisticas)
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
 
 controle.forEach( (elemento) => { 
     elemento.addEventListener("click",(evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
+
+        
      
     })
 })
@@ -10,16 +50,32 @@ controle.forEach( (elemento) => {
 function manipulaDados(operacao, controle){ 
 
     const peca = controle.querySelector("[data-contador]")
+    
 
     if (operacao === "-"){
         peca.value = parseInt(peca.value)-1
     }else{
         peca.value = parseInt(peca.value)+1
     }
+    
+}
+
+
+
+function atualizaEstatisticas(peca){
+    
+  console.log(pecas[peca])
+  estatisticas.forEach( (elemento) => {
+    console.log(elemento.dataset.estatistica)
+    console.log(elemento.textContent)
+    elemento.textContent= parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    
+
+  })
 
 }
 
 
-//<<<<<<<<<ATENÇÃO>>>>>>>>>
-// FAÇA O DESAFIO PROPOSTO NA AULA
+//<<<<<<<<<ATENï¿½ï¿½O>>>>>>>>>
+// FAï¿½A O DESAFIO PROPOSTO NA AULA
 //https://cursos.alura.com.br/course/javascript-manipulando-dom/task/106409
